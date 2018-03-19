39f1ba188e657db0eb859b09d0e6a2c32fe757f5
Daniel Seeger <6361571+derseeger@users.noreply.github.com>
2018-03-19 11:17:42 +0100

    Update settings.py
    Update default pagination settings and enable session authentication (Needed for development environment)

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -129,13 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK = {
-    # ...
-
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 16,
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     )
-}
