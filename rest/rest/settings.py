c129511635f7e82de2f00fde773a8e436c78f61f
Daniel Seeger <6361571+derseeger@users.noreply.github.com>
2018-03-23 19:03:37 +0100

    Update settings.py

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 16,
     'DEFAULT_PERMISSION_CLASSES': (
@@ -138,3 +139,5 @@ STATIC_URL = '/static/'
         'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     )
+}
+
