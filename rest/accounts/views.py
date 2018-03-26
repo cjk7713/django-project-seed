d2bd1ccb3ebd3a533fcb1e68387b5f3b32a1b329
Daniel Seeger <6361571+derseeger@users.noreply.github.com>
2018-03-26 20:12:48 +0200

    Fix accounts update view

--- rest/accounts/views.py
+++ rest/accounts/views.py
@@ -41,3 +41,18 @@ class AccountView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+    
+        def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            if "password" in request.data:
+                instance.set_password(request.data['password'])
+                instance.save()
+            else:
+                serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
